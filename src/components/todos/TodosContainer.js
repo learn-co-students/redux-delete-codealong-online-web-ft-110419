f578ad38654b904c154ff2dcc9f464573a659562
MaryArbs <mary.arbachauskas@gmail.com>
2020-04-08 10:33:16 -0400

    Done.

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -1,11 +1,16 @@
+// ./src/components/todos/TodosContainer.js
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import Todo from './Todo'
-
+ 
 class TodosContainer extends Component {
-
-  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
-
+ 
+  renderTodos = () => {
+    return this.props.todos.map(todo => <Todo delete={this.props.delete} key={todo.id} todo={todo} />)
+  } //ToDo is receiving this.props.delete. key was based off the index provided by map--- was text={todo}, where did text go?
+  //props are coming from mapDispatchtoProps aka store? what about todo.id why not acces with this.props.todos 
+  
+ 
   render() {
     return(
       <div>
@@ -14,11 +19,17 @@ class TodosContainer extends Component {
     );
   }
 };
-
+ 
 const mapStateToProps = state => {
   return {
     todos: state.todos
   }
 }
-
-export default connect(mapStateToProps)(TodosContainer);
+ 
+const mapDispatchToProps = dispatch => {
+  return {
+    delete: todoText => dispatch({type: 'DELETE_TODO', payload: todoText })
+  }
+}
+ 
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
