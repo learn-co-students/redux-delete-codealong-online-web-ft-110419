2ef6e83bbdb2d01196ab104ca9254fc9057d3d7d
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-13 15:41:09 -0400

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,12 +1,19 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default function manageTodo(state = {
   todos: [],
 }, action) {
   console.log(action)
+  console.log(state)
   switch (action.type) {
     case 'ADD_TODO':
-
-      return { todos: state.todos.concat(action.payload.text) };
-
+      const todo = {
+        id: uuidv4(),
+        text: action.payload.text
+      };
+      return { todos: state.todos.concat(todo) };
+    case 'DELETE_TODO':
+      return { todos: state.todos.filter(todo => todo.id !== action.payload) }
     default:
       return state;
   }
